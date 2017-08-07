a252f6974ce2555a073685f8aca12b58cf675f23
BHess2653 <GM_Dahaka@BHMac.local>
2017-08-06 22:37:32 -0400

    created Item in components

---
+++ src/components/Item/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {deleteItem} from '../../actions'
+
+class Item extends React.Components{
+  render(){
+    return (
+      <tr>
+        <td>
+          {this.props.item}
+        </td>
+        <td>
+          <button onClick={() => {this.props.deleteItem(this.props.id)}}>Delete</button>
+        </td>
+      </tr>
+    )
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({deleteItem}, dispatch);
+}
+
+export default connect(() => {return {};}, mapDispatchToProps)(Item);
