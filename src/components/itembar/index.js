cc75c91be387e4b5a4dd72ba9919d040b6908b8c
BHess2653 <GM_Dahaka@BHMac.local>
2017-08-06 22:25:48 -0400

    updated itembars index.js

--- src/components/itembar/index.js
+++ src/components/itembar/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'react-redux'
+import {addItem} from '../../actions'
 
-export default class ItemBar extends React.Component{
+class ItemBar extends React.Component{
   render(){
     return (
       <div>
         <input type="text" ref="task" placeholder="Add item here" />
-        <button>Add Item</button>
+        <button onClick={() => this.props.addItem(this.refs.item.value)}>Add Item</button>
       </div>
     );
   }
+};
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({addItem}, dispatch);
 }
+
+export default connect(() => {}, mapDispatchToProps)(ItemBar);
